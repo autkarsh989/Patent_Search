b840ea074af7d633a7bd9ddbf2029ff3c4857d12
satyam pathak <satyampathak67@gmail.com>
2024-06-15 11:11:32 +0530

    updated code

--- search/views.py
+++ search/views.py
@@ -4,10 +4,7 @@ import google.generativeai as genai
 from django.http import HttpResponseRedirect, JsonResponse
 from .newsc import scrape_patent_data
 import os
-import http.client
-import json
 genai.configure(api_key="AIzaSyCjr-30vvDZoejP_MDDvhYbWCdLw_2XPME")
-
 
 
 def search_patents(request):
@@ -19,11 +16,12 @@ def search_patents(request):
         model = genai.GenerativeModel("gemini-pro")
         chat = model.start_chat()
         res = chat.send_message(text+check_text)
-        scrape_patent_data(res.text)
+        print(res.text)
+        data=scrape_patent_data(res.text)
+        print(data)
         prompt="commpare both idea and tell if any thing is common in them i am giving you abstrat idea of both the project with intentiono of identifying if my idea is different and can be put to publish and patent tell your opinion if they are similar and what way before this prompt the idea which is ours is given and after this text all the idea which are on google patent is there  "
-        with open("data.txt","r+",encoding="utf-8") as file:
-            data = file.read()
         response = chat.send_message(text+prompt+data)
+        print(response.text)
         return render(request, 'search_results.html', {'results': response.text})
     elif request.method == 'GET':
         return render(request, 'search_form.html')
